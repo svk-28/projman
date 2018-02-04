2206b153f32dc216eb5122311379abab3985b8bd
Sergey Kalinin <banzaj28@yandex.ru>
2018-02-04 16:56:41 +0300

    Remove some files

--- hlp/ru/mail.tcl
+++
@@ -1,7 +0,0 @@
-######################################################
-#		Terminal
-#	Distributed under GNU Public License
-# Author: Sergey Kalinin svk@amur.dti.ru
-# Copyright (c) "DTi", 2003, http://www.amur.dti.ru
-######################################################
-
