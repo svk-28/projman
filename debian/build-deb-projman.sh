288cea7290d783dc804d40b74c0bbb9c964b901d
svkalinin <svkalinin@samsonpost.ru>
2022-08-12 15:21:04 +0300

    Добавлена запись номера сборки (build) в исходник, при сборке deb-пакета

--- debian/build-deb-projman.sh
+++ debian/build-deb-projman.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-cd projman
+cd ../
 
 VERSION=$(grep Version projman.tcl | grep -oE '\b[0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2}\b')
 RELEASE=$(grep Release projman.tcl | grep -oE '\b[0-9A-Za-z]{1,3}\b')
+BUILD_DATE=$(date +%d%m%Y%H%M%S)
+TXT="# Build: ${BUILD_DATE}"
+
+sed -i "/# Build:.*/c$TXT" projman.tcl
 
 cp projman.tcl projman
+
 
 sed -i "s+^set\ dir(lib)+set\ dir(lib)\ /usr/share/projman/lib ;#+g" projman
    
 sed -i "s+\[pwd\]+/usr/share/projman+g" projman
-   
+
+
 tar czf ../projman_${VERSION}.orig.tar.gz .
 
 dpkg-buildpackage
