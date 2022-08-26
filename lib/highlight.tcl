f6d69cfe9e3140503c8f89ce3c3234c316976066
svkalinin <svkalinin@samsonpost.ru>
2022-08-26 16:26:03 +0300

    Исправлена подсветка ключевых слов tcl  и добавлены виджеты

--- lib/highlight.tcl
+++ lib/highlight.tcl
@@ -6,9 +6,9 @@
 ######################################################
 namespace eval Highlight {} {
     proc TCL {txt} {
-        ctext::addHighlightClass $txt widgets purple  [list ctext button label text frame toplevel  scrollbar checkbutton canvas listbox menu menubar menubutton  radiobutton scale entry message tk_chooseDir tk_getSaveFile  tk_getOpenFile tk_chooseColor tk_optionMenu]
         ctext::addHighlightClassForRegexp $txt flags orange {\s-[a-zA-Z]+}
         ctext::addHighlightClass $txt stackControl #19a2a6 [info commands]
+        ctext::addHighlightClass $txt widgets #9d468d [list canvas ctext button entry label text labelframe frame toplevel scrollbar checkbutton canvas listbox menu menubar menubutton  radiobutton scale entry message tk_chooseDir tk_getSaveFile  tk_getOpenFile tk_chooseColor tk_optionMenu ttk::button ttk::checkbutton ttk::combobox ttk::entry ttk::frame ttk::intro ttk::label ttk::labelframe ttk::menubutton ttk::treeview ttk::notebook ttk::panedwindow ttk::progressbar ttk::radiobutton ttk::scale ttk::scrollbar ttk::separator ttk::sizegrip ttk::spinbox ]
         ctext::addHighlightClassWithOnlyCharStart $txt vars #4471ca "\$"
         ctext::addHighlightClass $txt variable_funcs gold {set global variable unset}
         ctext::addHighlightClassForSpecialChars $txt brackets green {[]{}()}
@@ -32,7 +32,7 @@ namespace eval Highlight {} {
     
     proc SH {txt} {
         ctext::addHighlightClassForRegexp $txt flags orange {-+[a-zA-Z\-_]+}
-        ctext::addHighlightClass $txt stackControl #19a2a6 {if fi else elseif then while case esac do in exit source echo package}
+        ctext::addHighlightClass $txt stackControl #19a2a6 {if fi else elseif then while case esac do in exit source echo package mkdir ls rm sed awk grep date jq zip tar gzip mount umount test make curl git iconv less gcc scp rsync cut tr}
         ctext::addHighlightClassWithOnlyCharStart $txt vars #4471ca "\$"
         ctext::addHighlightClassForRegexp $txt vars_extended #4471ca {\$\{[a-zA-Z0-9\_\-:\./\$\{\}]+\}}
         ctext::addHighlightClass $txt variable_funcs gold {set export}
